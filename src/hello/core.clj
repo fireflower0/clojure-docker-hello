e01c04a1adb728b5f4ada20022b100550af40493
fireflower0 <fireflower0@hotmail.co.jp>
2021-06-16 07:33:29 +0900

    Fixed a core.clj

--- src/hello/core.clj
+++ src/hello/core.clj
@@ -7,15 +7,30 @@
 
 (defn ok [body] {:status 200 :body body})
 
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found</1>"})
+
 (defn html [res]
   (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
 
 (defn view-index [req] "<h1>Index</h1>")
+(defn view-about [req] "<h1>About</h1>")
 
 (defn index [req] (-> (view-index req) ok html))
+(defn about [req] (-> (view-about req) ok html))
+
+(def routes {"/" index "/about" about})
+
+(defn match-route [uri]
+  (get routes uri))
 
 (defn handler [req]
-  (index req))
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
 
 (defn start-server []
   (when-not @server
